a7129ff63c419db9c1844bde7909da2aad4fe5ac
William Withers <william.k.withers@gmail.com>
2025-02-06 17:13:09 -0600

    test commit

--- backend/src/main/java/com/william/backend/data/Item.java
+++ backend/src/main/java/com/william/backend/data/Item.java
@@ -4,7 +4,6 @@ import java.util.Collection;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
