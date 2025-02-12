1a724ba27db09c8b073ca32a22138d98462975c4
William Withers <william.k.withers@gmail.com>
2025-02-12 15:21:42 -0600

    added loading screen to list inventory view

--- frontend/sis-frontend/src/components/ListInventoryComponent.jsx
+++ frontend/sis-frontend/src/components/ListInventoryComponent.jsx
@@ -8,8 +8,10 @@ import { listInventories } from '../services/InventoryService';
 import CardText from 'react-bootstrap/esm/CardText';
 import { listCategories } from '../services/CategoryService';
 import classnames from 'classnames';
+import LoadingScreen from './LoadingScreen';
 
 const ListInventoryComponent = () => {
+const [isLoading, setIsLoading] = useState(true);
 
 const[ALL_INVENTORIES, setALL_INVENTORIES] = useState([]);
 const [activeElement, setActiveElement] = useState(0);
@@ -58,6 +60,8 @@ function getAllInventories() {
         setInventories(response.data);
         setALL_INVENTORIES(response.data);
         console.log(response.data);
+
+        setIsLoading(false);
         
     }).catch(error => {
         console.error(error);
@@ -100,34 +104,43 @@ function handleCategoryTabEvent(categoryId, index) {
 
 
   return (
-    <div className='container'>
-        <div className='outer'>
-            {categories.map((category, index) =>
-                <div key={category.id}
-                     onClick={() => handleCategoryTabEvent(category.id, index)}
-                     className={activeElement === index ? 'bb' : ''}>
-                    {category.name}
+
+    <div>
+        {isLoading ? (<LoadingScreen /> ) : (
+        <div className='container'>
+            <div className='outer'>
+                {categories.map((category, index) =>
+                    <div key={category.id}
+                        onClick={() => handleCategoryTabEvent(category.id, index)}
+                        className={activeElement === index ? 'bb' : ''}>
+                        {category.name}
+                    </div>
+                )}
+            </div>
+            {inventories.map(inventory => 
+            <Card key={inventory.id} className='my-3' onClick={() => cardClickEvent(inventory.id)}>
+            <Card.Body className='d-flex justify-content-between align-items-center'>
+                <div>
+                    <Card.Title>{inventory.item.name}</Card.Title>
+                    <Card.Text className='mb-2'>{inventory.item.category != null ? inventory.item.category.name : 'Uncategorized'}</Card.Text>
+                    <Card.Text>{inventory.item.url != '' ? <a href={inventory.item.url} target='_tab'>Link</a> : ''}</Card.Text>
                 </div>
+                <div className='d-flex flex-column align-items-end'>
+                    <Card.Title>{inventory.quantity}</Card.Title>
+                    <Card.Text>{inventory.unitOfMeasure}</Card.Text>
+                </div>
+                
+                {/* <Button variant="primary">Go somewhere</Button> */}
+            </Card.Body>
+            </Card> 
             )}
         </div>
-        {inventories.map(inventory => 
-           <Card key={inventory.id} className='my-3' onClick={() => cardClickEvent(inventory.id)}>
-           <Card.Body className='d-flex justify-content-between align-items-center'>
-            <div>
-                <Card.Title>{inventory.item.name}</Card.Title>
-                <Card.Text className='mb-2'>{inventory.item.category != null ? inventory.item.category.name : 'Uncategorized'}</Card.Text>
-                <Card.Text>{inventory.item.url != '' ? <a href={inventory.item.url} target='_tab'>Link</a> : ''}</Card.Text>
-            </div>
-            <div className='d-flex flex-column align-items-end'>
-                <Card.Title>{inventory.quantity}</Card.Title>
-                <Card.Text>{inventory.unitOfMeasure}</Card.Text>
-            </div>
-             
-             {/* <Button variant="primary">Go somewhere</Button> */}
-           </Card.Body>
-         </Card> 
-        )}
+    )}
+
     </div>
+    
+
+    
   )
 }
 
